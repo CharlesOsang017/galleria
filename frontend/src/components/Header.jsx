50320a92ee31881e75925498bd6c3a91cd9e1cb3
CharlesOsang017 <charlesosango02@gmail.com>
2024-11-01 16:17:55 +0300

    Complete hero section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='flex justify-between shadow-md px-6 py-3 items-center'>
+    <header className='flex justify-between shadow-md px-6 py-2 items-center'>
         <div className="logo">
            <img src="/public/svg_lg.png" alt="gallery_logo"  className='md:h-20 md:w-20 h-10 w-10'/>           
         </div>
